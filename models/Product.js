5b82ceee156699abf8a5650eca1e447855eea381
Intoloshubham <shubham.sahu@intoloindia.com>
2022-12-31 17:40:16 +0530

    "12 31 2022"

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
     user_id:{type:String,required:true},
     product_id:{ type: ObjectId, required:true},
-    service_used_in:{type:String,required:true}
+    service_used_in:{type:String,required:true},
+    status:{type:Boolean,default:false}
 })
 
 export default mongoose.model('Product',productSchema,'products');
