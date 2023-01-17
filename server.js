b3a2d3bac7cbcf14691a37286b094c2088f0006b
Intoloshubham <shubham.sahu@intoloindia.com>
2023-01-17 14:58:37 +0530

    "1 17 2023 cmt"

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import {socketConn,io} from "./utils/SocketService.js";
 import constants from "./constants/index.js";
 
 
-
+socketConn('connected');
 const app = express();
 
 
@@ -36,7 +36,7 @@ app.use(
 // app.use(express.json())
 app.use(express.json({ limit: "10mb" }));
 
-// Load Routes
+// Load Routes 
 app.use("/api", routes);
 app.use("/uploads", express.static("uploads"));
 
